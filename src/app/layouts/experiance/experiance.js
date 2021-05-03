ce4363f0f3d842490e4dc93aec7eaf21d52485dd
Sam-Shpakov <skaymant@yandex.ru>
2021-05-03 17:05:50 +0300

    feat: update info and change size cards

--- src/app/layouts/experiance/experiance.js
+++ src/app/layouts/experiance/experiance.js
@@ -8,14 +8,23 @@ let experiance = `
         <i class="icon-point"></i>
         <div class="timeline-content">
           <div class="timeline-header">
+            <h3>Mentor</h3>
+          </div>
+          <p class="sub-heading">The Rolling Scopes School</p>
+          <p class="sub-heading">April 2021 - Present Time</p>
+          <p class="content-p">Mentoring</p>
+        </div>
+      </li>
+      <li class="anim-items">
+        <i class="icon-point"></i>
+        <div class="timeline-content">
+          <div class="timeline-header">
             <h3>Freelancer</h3>
           </div>
           <p class="sub-heading">Any companies</p>
-          <p class="sub-heading">May 2020 - April 2021</p>
+          <p class="sub-heading">May 2020 - Present Time</p>
           <p class="content-p">
-            Main responsibilities:
-            layout
-            development from scratch
+            Main responsibilities: layout development from scratch
             scripting
           </p>
         </div>
--- src/app/layouts/skills/skills.js
+++ src/app/layouts/skills/skills.js
@@ -1,97 +1,36 @@
 let skills = `
 <section class="skills" id="skills">
-<div id='parallax-image'>
+<div id="parallax-image">
   <h4 class="skills__name anim-items">Skills</h4>
   <div class="wrapper skills__wrapper">
     <div class="skill-card">
-      <h5 class="skill-title">Technologies</h5>
-      <div class="skill-progress-div">
-        <p>JavaScript<span>90%</span></p>
-        <div class="skill-progress" data-percent="90%">
-          <div class="skill-determinate" style="width: 90%;">
-            <i class="skill-determinate-circle anim-items"></i>
-          </div>
-        </div>
-      </div>
-
-      <div class="skill-progress-div">
-        <p>HTML/CSS/SASS(SCSS)<span>90%</span></p>
-        <div class="progress skill-progress" data-percent="90%">
-          <div class="skill-determinate" style="width: 90%;">
-            <i class="skill-determinate-circle anim-items"></i>
-          </div>
-        </div>
-      </div>
-      <div class="skill-progress-div">
-        <p>GraphQL, Apollo GraphQL<span>80%</span></p>
-        <div class="skill-progress" data-percent="80%">
-          <div class="skill-determinate" style="width: 80%;">
-            <i class="skill-determinate-circle anim-items"></i>
-          </div>
-        </div>
-      </div>
-      <div class="skill-progress-div">
-        <p>Typescript<span>70%</span></p>
-        <div class="skill-progress" data-percent="70%">
-          <div class="skill-determinate" style="width: 70%;">
-            <i class="skill-determinate-circle anim-items"></i>
-          </div>
-        </div>
-      </div>
-      <div class="skill-progress-div">
-        <p>React+Redux<span>50%</span></p>
-        <div class="skill-progress" data-percent="50%">
-          <div class="skill-determinate" style="width: 50%;">
-            <i class="skill-determinate-circle anim-items"></i>
-          </div>
-        </div>
-      </div>
+      <h5 class="skill-title">Tools & Technologies</h5>
+      <ul class="skill-progress-div">
+        <li class="anim-items"><div class="skill-name">React</div></li>
+        <li class="anim-items"><div class="skill-name">TypeScript</div></li>
+        <li class="anim-items"><div class="skill-name">Redux</div></li>
+        <li class="anim-items"><div class="skill-name">GraphQL, Apollo GraphQL</div></li>
+        <li class="anim-items"><div class="skill-name">Webpack, Git, Trello</div></li>
+        <li class="anim-items"><div class="skill-name">JavaScript ES6/ES7/ES8/ES9/ES10</div></li>
+        <li class="anim-items"><div class="skill-name">CSS, SCSS, SASS, Styled-components</div></li>
+        <li class="anim-items"><div class="skill-name">Next.js</div></li>
+        <li class="anim-items"><div class="skill-name">REST APIs</div></li>
+      </ul>
     </div>
     <div class="skill-card">
-      <h5 class="skill-title">Industry Knowledge</h5>
-      <div class="skill-progress-div">
-        <p>Scrum<span>80%</span></p>
-        <div class="progress skill-progress" data-percent="80%">
-          <div class="determinate skill-determinate" style="width: 80%;">
-            <i class="skill-determinate-circle anim-items"></i>
-          </div>
-        </div>
-      </div>
-
-      <div class="skill-progress-div">
-        <p>Problem solving<span>90%</span></p>
-        <div class="progress skill-progress" data-percent="90%">
-          <div class="determinate skill-determinate" style="width: 90%;">
-            <i class="skill-determinate-circle anim-items"></i>
-          </div>
-        </div>
-      </div>
-      <div class="skill-progress-div">
-        <p>Leadership<span>80%</span></p>
-        <div class="progress skill-progress" data-percent="80%">
-          <div class="determinate skill-determinate" style="width: 80%;">
-            <i class="skill-determinate-circle anim-items"></i>
-          </div>
-        </div>
-      </div>
-      <div class="skill-progress-div">
-        <p>Analytical Skill<span>80%</span></p>
-        <div class="progress skill-progress" data-percent="80%">
-          <div class="determinate skill-determinate" style="width: 80%;">
-            <i class="skill-determinate-circle anim-items"></i>
-          </div>
-        </div>
-      </div>
-      <div class="skill-progress-div">
-        <p>Creativity<span>80%</span></p>
-        <div class="progress skill-progress" data-percent="80%">
-          <div class="determinate skill-determinate" style="width: 80%;">
-            <i class="skill-determinate-circle anim-items"></i>
-          </div>
-        </div>
-      </div>
+      <h5 class="skill-title">Other Skills</h5>
+      <ul class="skill-progress-div">
+        <li class="anim-items"><div class="skill-name">Object-Oriented Programming (OOP)</div></li>
+        <li class="anim-items"><div class="skill-name">Scrum</div></li>
+        <li class="anim-items"><div class="skill-name">React Hooks</div></li>
+        <li class="anim-items"><div class="skill-name">Express.js</div></li>
+        <li class="anim-items"><div class="skill-name">Docker Products</div></li>
+        <li class="anim-items"><div class="skill-name">Back-End Web Development</div></li>
+        <li class="anim-items"><div class="skill-name">Project Management</div></li>
+        <li class="anim-items"><div class="skill-name">Agile Methodologies</div></li>
+        <li class="anim-items"><div class="skill-name">Problem Solving</div></li>
+      </ul>
     </div>
-  </div>
 </div>
 </section>
 `;

8f81b6ad97b01eb559a530c2cc318735bbb56fce
Sam-Shpakov <skaymant@yandex.ru>
2021-05-04 12:57:45 +0300

    feat: fix menu

--- src/app/layouts/header/header.js
+++ src/app/layouts/header/header.js
@@ -2,8 +2,8 @@ let header = `
 <header class="header">
 <div class="wrapper header_wrapper">
   <a class="header__logo">
-    <h1 class="header__logo-title">James Bond</h1>
-    <div class="header__logo-subtitle">Secret agent MI6</div>
+    <h1 class="header__logo-title">Sam Shpakov</h1>
+    <div class="header__logo-subtitle">Frontend Engineer</div>
   </a>
   <div class="header__menu">
     <input type="checkbox" id="menu__toggle" class="menu__checkbox" />
@@ -14,29 +14,33 @@ let header = `
       <div class="navigation__container">
         <ul class="navigation">
           <li class="navigation__item">
-            <a class="navigation__item_link">about</a>
+            <a href="/#about" class="navigation__item_link">about</a>
           </li>
           <li class="navigation__item">
-            <a class="navigation__item_link">experiance</a>
+            <a href="/#portfolios" class="navigation__item_link"
+              >portfolios</a
+            >
           </li>
           <li class="navigation__item">
-            <a class="navigation__item_link">skills</a>
-          </li>
-          <li class="navigation__item"><a class="navigation__item_link">portfolios</a></li>
-          <li class="navigation__item">
-            <a class="navigation__item_link">education</a>
+            <a href="/#skills" class="navigation__item_link">skills</a>
           </li>
           <li class="navigation__item">
-            <a class="navigation__item_link">interest</a>
+            <a href="/#experiance" class="navigation__item_link"
+              >experiance</a
+            >
           </li>
           <li class="navigation__item">
-            <a class="navigation__item_link">contact</a>
+            <a href="/#education" class="navigation__item_link"
+              >education</a
+            >
+          </li>
+          <li class="navigation__item">
+            <a href="/#contact" class="navigation__item_link">contact</a>
           </li>
         </ul>
       </div>
     </div>
   </div>
-</div>
 </div>
 </header>
 `;

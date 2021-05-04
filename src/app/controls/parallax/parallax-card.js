2308f28e280733aaf3df4a3c157f4c991efa1db2
SkaymanT <SkaymanT@yandex.ru>
2021-05-04 22:46:08 +0300

    feat: fix for safari

--- src/app/controls/parallax/parallax-card.js
+++ src/app/controls/parallax/parallax-card.js
@@ -7,10 +7,26 @@ export function parallaxCardHandler(element) {
 function transformElement(elementOne, elementTwo) {
   let myPanel = elementOne;
   let subpanel = elementTwo;
+
+  let prefix = '';
+  if (navigator.userAgent.indexOf('Edg') != -1) {
+    prefix = '-webkit-';
+  } else if (navigator.userAgent.indexOf('YaBrowser') != -1) {
+    prefix = '-webkit-';
+  } else if (navigator.userAgent.indexOf('Safari') != -1) {
+    prefix = '';
+  } else if (navigator.userAgent.indexOf('Firefox') != -1) {
+    prefix = '-moz-';
+  } else {
+    prefix = '-webkit-';
+  }
+  if (prefix === '') {
+    return;
+  }
+
   return function (mouseEvent) {
     let mouseX, mouseY;
-
-    let transformAmount = 5;
+    let transformAmount = 10;
 
     mouseX = mouseEvent.pageX;
     mouseY = mouseEvent.pageY;
@@ -21,37 +37,49 @@ function transformElement(elementOne, elementTwo) {
     const percentX = (mouseX - centerX) / (myPanel.clientWidth / 2);
     const percentY = -((mouseY - centerY) / (myPanel.clientHeight / 2));
 
-    subpanel.style.transform =
-      "perspective(400px) rotateY(" +
-      percentX * transformAmount +
-      "deg) rotateX(" +
-      percentY * transformAmount +
-      "deg)" +
-      "scale(1.3)";
-
-    subpanel.style.zIndex = "2";
-    subpanel.style.cursor = "pointer";
+    subpanel.setAttribute(
+      'style',
+      `${prefix}transform: perspective(400px) rotateY(${
+        percentX * transformAmount
+      }deg) rotateX(${
+        percentY * transformAmount
+      }deg) scale(1.3); z-index: 2; cursor: pointer;`
+    );
   };
 }
 function handleMouseEnter(element) {
   let subpanel = element;
   return function () {
     setTimeout(() => {
-      subpanel.style.transition = "";
+      subpanel.style.transition = '';
     }, 100);
-    subpanel.style.transition = "transform 0.1s";
+    subpanel.style.transition = 'transform 0.1s';
   };
 }
 
 function handleMouseLeave(element) {
   let subpanel = element;
+  let prefix = '';
+  if (navigator.userAgent.indexOf('Edg') != -1) {
+    prefix = '-webkit-';
+  } else if (navigator.userAgent.indexOf('YaBrowser') != -1) {
+    prefix = '-webkit-';
+  } else if (navigator.userAgent.indexOf('Safari') != -1) {
+    prefix = '-webkit-';
+  } else if (navigator.userAgent.indexOf('Firefox') != -1) {
+    prefix = '-webkit-';
+  } else {
+    prefix = '-webkit-';
+  }
   return function () {
-    subpanel.style.transition = "transform 0.1s";
+    subpanel.style.transition = 'transform 0.1s';
     setTimeout(() => {
-      subpanel.style.transition = "";
+      subpanel.style.transition = '';
     }, 100);
 
-    subpanel.style.transform = "perspective(400px) rotateY(0deg) rotateX(0deg)";
-    subpanel.style.zIndex = "0";
+    subpanel.setAttribute(
+      'style',
+      `${prefix}transform: perspective(400px) rotateY(0deg) rotateX(0deg); z-index: 0; cursor: pointer;`
+    );
   };
 }
